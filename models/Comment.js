ac6b7b8305c76c6b3dae8b3aa3fc841eefd8f901
abhiram11 <abhiramsatpute@gmail.com>
2022-10-18 13:59:36 +0530

    models made, validations set, now working on gql init

---
+++ models/Comment.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    postId: { type: String, required: true },
+    comment: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("comment", commentSchema);
